8eca2bf6badd0c8041fa14965cfafb9d4ff45aa7
nabeelktr <nabee446@gmail.com>
2024-04-08 13:01:13 +0530

    added order apis

---
+++ redux/features/order/orderApi.ts
@@ -0,0 +1,49 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const ordersApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllOrders: builder.query({
+      query: (type) => ({
+        url: "order/get-orders",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+
+    getStripePublishKey: builder.query({
+      query: () => ({
+        url: "order/stripe-publishkey",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+
+    createPaymentIntent: builder.mutation({
+      query: (amount) => ({
+        url: "order/payment",
+        method: "POST",
+        body: { amount },
+        credentials: "include" as const,
+      }),
+    }),
+
+    createOrder: builder.mutation({
+      query: ({ courseId, payment_info }) => ({
+        url: "order/create-order",
+        body: {
+          courseId,
+          payment_info,
+        },
+        method: "POST",
+        credentials: "include" as const,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllOrdersQuery,
+  useCreateOrderMutation,
+  useGetStripePublishKeyQuery,
+  useCreatePaymentIntentMutation,
+} = ordersApi;
