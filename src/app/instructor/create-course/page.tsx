80b1dfd2255901318a94496a1e67cc407d1e0459
nabeelktr <nabee446@gmail.com>
2024-04-15 09:29:08 +0530

    header added in instructor

--- src/app/instructor/create-course/page.tsx
+++ src/app/instructor/create-course/page.tsx
@@ -4,10 +4,13 @@ import CreateCourse from "../../../components/Instructor/CreateCourse/CreateCour
 import React from "react";
 import Heading from "../../../utils/Heading";
 import Sidebar from "../../../components/Instructor/Sidebar/Sidebar";
+import DashboardHero from "@/components/Admin/DashboardHero";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
 const CreateCoursePage = (props: Props) => {
+  const {user} = useSelector((state: any) => state.auth)
   return (
     <InstructorProtected>
       <Heading
@@ -17,6 +20,9 @@ const CreateCoursePage = (props: Props) => {
       />
       <div className="flex mx-auto z-[9999]">
         <div className="mx-auto 800px:w-[80%] 800px:px-[100px] pl-10 ml-10  mr-0 800px:mr-16">
+        <div className="z-[99] mb-10">
+           {user && <DashboardHero instructorId={user.id}/>}
+            </div>
           <CreateCourse />
         </div>
         <Sidebar active={0} />
