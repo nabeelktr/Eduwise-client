03e174cb755764a65498850812f1da432832968a
nabeelktr <nabee446@gmail.com>
2024-04-25 13:18:11 +0530

    removed trailingSlash from config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'eduwise.s3.ap-south-1.amazonaws.com', 'firebasestorage.googleapis.com', "ec2-13-201-3-176.ap-south-1.compute.amazonaws.com", "wyntra.shop"],
     },
-    trailingSlash: true,
 };
 
 export default nextConfig;
