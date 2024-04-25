0937711c1571891ee2b6fb62caaceea3edff0d02
nabeelktr <nabee446@gmail.com>
2024-04-25 09:47:55 +0530

    updated socket configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'eduwise.s3.ap-south-1.amazonaws.com', 'firebasestorage.googleapis.com', "ec2-13-201-3-176.ap-south-1.compute.amazonaws.com"],
+        domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'eduwise.s3.ap-south-1.amazonaws.com', 'firebasestorage.googleapis.com', "ec2-13-201-3-176.ap-south-1.compute.amazonaws.com", "wyntra.shop"],
     },
     trailingSlash: true,
 };
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,3 +1,3 @@
 import socketIO from "socket.io-client";
 const EndPoint = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
-export const socketId = socketIO(EndPoint,{secure:true});
+export const socketId = socketIO(EndPoint,{ transports: ["websocket"] });
